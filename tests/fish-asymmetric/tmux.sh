866ce963a0faa3d163e08bc17a6e87a4dcb78c4d
Vitor buxbaum <vitorbuxbaum@gmail.com>
2018-05-29 23:14:58 -0300

    correção em tests/fish-ecmp

--- tests/fish-asymmetric/tmux.sh
+++ tests/fish-asymmetric/tmux.sh
@@ -5,6 +5,6 @@ exe="python3.5 ../../router.pyc"
 exe="../../router.py"
 
 for i in $(seq 1 6) ; do
-    tmux split-pane -v $exe --addr "127.0.1.$i" --startup-commands "$i.txt" &
+    tmux % -v $exe --addr "127.0.1.$i" --startup-commands "$i.txt" &
     tmux select-layout even-vertical
 done
